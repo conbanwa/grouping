bf0ae8f54ab3b22d5f7242cfb3d9eae6574a4d24
conbanwa <j6j7@qq.com>
2021-04-23 15:50:47 +0800

    text+=

---
+++ js/settings.js
@@ -0,0 +1,43 @@
+function outport() {
+    let text = ""
+    zTree.getNodes().forEach(nest);
+
+    // 下载文件方法
+    var funDownload = function(content, filename) {
+        var eleLink = document.createElement('a');
+        eleLink.download = filename;
+        eleLink.style.display = 'none';
+        // 字符内容转变成blob地址
+        var blob = new Blob([content]);
+        eleLink.href = URL.createObjectURL(blob);
+        // 触发点击
+        document.body.appendChild(eleLink);
+        eleLink.click();
+        // 然后移除
+        document.body.removeChild(eleLink);
+    };
+    if ('download' in document.createElement('a')) {
+        funDownload(text, '导出结果.txt');
+    } else {
+        alert('浏览器不支持');
+    }
+
+    function nest(n) {
+        text += (n.name)
+        if (n.pId != null) {
+            let members = app.get(n.tId)
+            if (members && members.in) {
+                let l = members.in.length
+                if (l) {
+                    for (i = 0; i < l; i++) {
+                        text += (members.in[i].name)
+                    }
+                }
+            }
+        }
+        if (n.children) {
+            for (let i = 0, l = n.children.length; i < l; i++)
+                nest(n.children[i])
+        }
+    }
+}
